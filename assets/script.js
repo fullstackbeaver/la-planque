eabdc1a142f7394349846fe6e866d80873cfe4e9
Lionel <fullstackbeaver@gmail.com>
2026-02-16 08:51:25 +0100

    style: clean script.js

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,16 @@
 window.onload = () => {
   const nav = document.querySelector('.hero nav');
   const body = document.querySelector('body');
-  console.log("...", nav, window.innerWidth < 1024)
 
   body.addEventListener('click', (e) => {
     if (window.innerWidth >= 1024) return;
     const rect = body.getBoundingClientRect();
-    // const style = window.getComputedStyle(nav, '::before');
-
-    // Récupérer les dimensions du ::before
-    // const beforeWidth = parseInt(style.width);
-    // const beforeHeight = parseInt(style.height);
-
-    // Calculer la position relative au nav
-
 
     const clickX = e.clientX - rect.left;
-    const clickY = e.clientY;// - window.scrollY;
-    console.log(clickX, clickY, e.clientY, window.scrollY)
+    const clickY = e.clientY;
 
-    // Zone du burger (à ajuster selon votre CSS)
     if (
-      clickX >= rect.width - 60 && // 60px depuis la droite
+      clickX >= rect.width - 60 &&
       clickX <= rect.width - 20 &&
       clickY >= 20 &&
       clickY <= 60
